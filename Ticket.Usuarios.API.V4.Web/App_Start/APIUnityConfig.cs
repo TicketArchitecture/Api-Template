612be8107f4b70efafc6f39406240cd9eda26b5a
Alexander <alexander.teixeira@consulting-for.edenred.com>
2015-12-30 18:02:25 -0200

    faltando acertar o post

---
+++ Ticket.Usuarios.API.V4.Web/App_Start/APIUnityConfig.cs
@@ -0,0 +1,18 @@
+using Microsoft.Practices.Unity;
+using System.Web.Http;
+using Unity.WebApi;
+using Ticket.API.Shared.Infrastructure;
+
+namespace Ticket.Usuarios.API.V4.Web
+{
+    public static class APIUnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            UnityConfig.RegistrarTudo();
+
+            //isto só serve para web API
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(UnityConfig.Container);
+        }
+    }
+}
