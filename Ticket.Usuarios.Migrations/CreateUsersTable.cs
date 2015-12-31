cefed215dc8116c4f9eeddcfc81f71c6854fbce9
Alexander <alexander.teixeira@consulting-for.edenred.com>
2015-12-31 11:57:57 -0200

    tudo funcionando

---
+++ Ticket.Usuarios.Migrations/CreateUsersTable.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace Ticket.Usuarios.Migrations
+{
+    [Migration(1)]
+    public class CreateUsersTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Usuarios")
+            .WithColumn("id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("nome_completo").AsString().NotNullable()
+            .WithColumn("email").AsString().NotNullable()
+            .WithColumn("cpf").AsString().WithDefaultValue("nao_informado")
+            .WithColumn("aceita_mkt").AsBoolean().NotNullable()
+            .WithColumn("status_validacao").AsInt32().WithDefaultValue(0)
+            .WithColumn("numero_telefone").AsString().WithDefaultValue("nao_informado")
+            .WithColumn("uuid_telefone").AsString().WithDefaultValue("nao_informado")
+            .WithColumn("telefone_validado").AsBoolean().WithDefaultValue(false)
+            .WithColumn("sistema_operacional_telefone").AsString().WithDefaultValue("nao_informado");
+
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Usuarios");
+        }
+    }
+}
