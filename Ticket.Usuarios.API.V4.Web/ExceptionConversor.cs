f17cc926a1afd6a175930070ea7d326d4f355f02
Alexander <alexander.teixeira@consulting-for.edenred.com>
2016-01-05 19:26:38 -0200

    tratando excessoes

---
+++ Ticket.Usuarios.API.V4.Web/ExceptionConversor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ModelBinding;
+using Ticket.API.Shared.Infrastructure;
+
+namespace Ticket.Usuarios.API.V4.Web
+{
+    static class ExceptionConversor
+    {
+
+        public static void ComplementModelStateErrors(ModelStateDictionary state, BusinessException exception)
+        {
+            foreach (var error in exception.Data as IDictionary<string, string>)
+                state.AddModelError(error.Key,error.Value); 
+        }
+    }
+}
